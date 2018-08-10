d67d0bef305112fc7a1e341de7632b0712acecbf
Vedran Karacic <karacicvedran@gmail.com>
2018-08-10 15:18:40 +0200

    Allow generate_discussion_topics cmd to accept course key argument.

--- lms/djangoapps/ed2go/management/commands/generate_discussion_topics.py
+++ lms/djangoapps/ed2go/management/commands/generate_discussion_topics.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.test import RequestFactory
 
+from opaque_keys.edx.keys import CourseKey
 from openedx.features.course_experience.utils import get_course_outline_block_tree
 from xmodule.modulestore.django import modulestore
 
@@ -10,12 +11,22 @@ class Command(BaseCommand):
     """
     Create Discussions topics from course chapter names.
     """
-
     def handle(self, *args, **options):
+        course_key = None
+        if len(args) == 1:
+            course_key = CourseKey.from_string(args[0])
+
+        module_store = modulestore()
+        if course_key:
+            course = module_store.get_course(course_key)
+            courses = [course]
+        else:
+            courses = module_store.get_courses()
+
         fake_request = RequestFactory().get(u'/')
         fake_request.user = User.objects.filter(is_superuser=True).first()
-        module_store = modulestore()
-        for course in module_store.get_courses():
+
+        for course in courses:
             course_block_tree = get_course_outline_block_tree(fake_request, str(course.id))
             for chapter in course_block_tree['children']:
                 key = chapter['display_name'].replace('.', ':')
