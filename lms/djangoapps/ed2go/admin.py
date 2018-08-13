76aea4bc2e8fc49daf7b818c4c0795a354d2c1b3
Vedran Karacic <karacicvedran@gmail.com>
2018-08-13 12:25:26 +0200

    Add field search and filters for ed2go admin panel.

--- lms/djangoapps/ed2go/admin.py
+++ lms/djangoapps/ed2go/admin.py
@@ -6,8 +6,11 @@ from ed2go.models import CompletionProfile, CourseSession
 @admin.register(CompletionProfile)
 class CompletionProfileAdmin(admin.ModelAdmin):
     list_display = ('pk', 'user', 'course_key', 'registration_key', 'active')
+    list_filter = ('user', 'course_key', 'active')
+    search_fields = ('user', 'course_key')
 
 
 @admin.register(CourseSession)
 class CourseSessionAdmin(admin.ModelAdmin):
     list_display = ('pk', 'user', 'course_key', 'created_at', 'last_activity_at', 'closed_at', 'active')
+    list_filter = ('user', 'course_key', 'active')
